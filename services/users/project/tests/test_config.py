e1d0a9f87022bf3d3f0d34419e7ad2aede4f4989
andreitch <andreitch.top@gmail.com>
2019-09-18 08:43:23 +0300

    debug the app

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -16,8 +16,11 @@ class TestDevelopmentConfig(TestCase):
     def test_app_is_development(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(current_app is None)
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI']
-                        == os.environ.get('DATABASE_URL'))
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_URL')
+        )
+        self.assertTrue(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestTestingConfig(TestCase):
@@ -29,8 +32,11 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI']
-                        == os.environ.get('DATABASE_TEST_URL'))
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_TEST_URL')
+        )
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestProductionConfig(TestCase):
@@ -41,6 +47,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 if __name__ == '__main__':
