a4aed2f79efef5ba94d39a3c4c16c1bced6f02e8
andreitch <andreitch.top@gmail.com>
2019-09-25 20:40:15 +0300

    Navbar and Form components with React Bootstrap

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import axios from "axios";
 
 import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
+import Form from "./components/Form"
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,13 @@ class App extends Component {
     this.state = {
       users: [],
       username: "",
-      email: ""
+      email: "",
+      title: "React App",
+      formData: {
+        username: "",
+        email: "",
+        password: ""
+      }
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -28,29 +36,39 @@ class App extends Component {
       username: this.state.username,
       email: this.state.email
     };
-    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
       .then(res => {
         this.getUsers();
         this.setState({ username: "", email: "" });
       })
-      .catch(err => { console.log(err); });
+      .catch(err => {
+        console.log(err);
+      });
   }
   componentDidMount() {
     this.getUsers();
   }
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-      .then(res => { this.setState({ users: res.data.data.users }); })
-      .catch(err => { console.log(err); });
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then(res => {
+        this.setState({ users: res.data.data.users });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <br />
-            <Switch>
-              <Route exact path="/" render={() => (
+      <div>
+        <NavBar title={this.state.title} />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <br />
+              <Switch>
+                <Route exact path="/" render={() => (
                   <div>
                     <h1>All Users</h1>
                     <hr />
@@ -58,16 +76,22 @@ class App extends Component {
                     <AddUser
                       username={this.state.username}
                       email={this.state.email}
-                      addUser={this.addUser}
                       handleChange={this.handleChange}
+                      addUser={this.addUser}
                     />
                     <br />
                     <UsersList users={this.state.users} />
                   </div>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-            </Switch>
+                )} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" render={() => (
+                    <Form formType={"Register"} formData={this.state.formData} />
+                )} />
+                <Route exact path="/login" render={() => (
+                    <Form formType={"Login"} formData={this.state.formData} />
+                )} />
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
